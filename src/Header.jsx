674219c60ba53b82a0f6c6b15583e644f052fc6f
Mohd Parvez <mohd.parvez@netmente.com>
2025-12-01 12:11:41 +0530

    React Project

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return(
+        <>
+            <header>
+                <ul>
+                    <li><a href="#">home</a></li>
+                    <li><a href="#">home</a></li>
+                    <li><a href="#">home</a></li>
+                    <li><a href="#">home</a></li>
+                </ul>
+            </header>
+        </>
+    )
+}
+
+export default Header;
